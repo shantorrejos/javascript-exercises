ed814ef2a3a958327a59d9d1bf91b52b8ca415a7
Shan Torrejos <117603433+shantorrejos@users.noreply.github.com>
2024-07-31 21:47:40 +0800

    "Solve" assignment number 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    const arr = str.split("");
+    const reversedString = [];
+    for (let index = 0; index = arr.length; index++) {
+        reversedString.push(arr.pop());
+    }
+    return reversedString.join("");
 
+    // I didnt know reverse method existed XDXDXDXDXD
+    
 };
 
 // Do not edit below this line
