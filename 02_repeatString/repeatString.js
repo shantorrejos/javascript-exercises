cedd2a1da44bcbda5016e53352f37c1c33818777
Shan Torrejos <117603433+shantorrejos@users.noreply.github.com>
2024-07-31 21:18:37 +0800

    Solve assignment 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let finalText= "";
+
+    if (num >= 0){
+        for (let index = 0; index < num ; index++) {
+            finalText += str;
+        }
+    
+        return finalText;
+    }else{
+        return "ERROR";
+    }
 
 };
 
