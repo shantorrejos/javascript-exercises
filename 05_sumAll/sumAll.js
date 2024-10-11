35c1da46e0e5bb5fd4bcea9832dec554546028d9
Shan Torrejos <117603433+shantorrejos@users.noreply.github.com>
2024-10-11 15:45:35 +0800

    Solve sumAll assignment

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(firstInt, secondInt) {
+    let sum = 0;
 
+    if (firstInt < 0 || secondInt < 0) {
+        return "ERROR";
+    }
+
+    if (!Number.isInteger(firstInt) || !Number.isInteger(secondInt)) {
+        return "ERROR"
+    }
+
+    if (firstInt > secondInt) {
+        let temp  = secondInt;
+        secondInt = firstInt;
+        firstInt = temp;
+    }
+
+    console.log(firstInt, secondInt)
+    for (let index = firstInt; index <= secondInt; index++) {
+        sum += index;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
