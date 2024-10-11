36b23b8e621b62947ed5397360753c0a89bb4ad0
Shan Torrejos <117603433+shantorrejos@users.noreply.github.com>
2024-10-11 15:55:05 +0800

    Solve tempConversion assignment

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(givenValue) {
+  let valueCelsius = (givenValue - 32) * 5 / 9;
+
+  if (!Number.isInteger(valueCelsius)) {
+    return (Math.round(valueCelsius * 10) / 10);
+  }
+  return valueCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(givenValue) {
+  let valueFahrenheit = (givenValue * 9/5) + 32
+  if (!Number.isInteger(valueFahrenheit)) {
+    return (Math.round(valueFahrenheit * 10) / 10);
+  }
+  return valueFahrenheit;
+
 };
 
 // Do not edit below this line
