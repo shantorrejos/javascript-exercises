213ed5de34676b11606c6898c765e1a4ef125130
Shan Torrejos <117603433+shantorrejos@users.noreply.github.com>
2024-10-11 15:45:46 +0800

    Solve leapYears assignment

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function(givenYear) {
+    if (givenYear % 100 === 0 && givenYear % 400 === 0) {
+        return true;
+    }
 
+    if (givenYear % 4 === 0 && !(givenYear % 100 === 0)) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
